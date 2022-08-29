029b4d1f51ce99c33cab7602024523309b907336
suhaidiaming <112393762+suhaidiaming@users.noreply.github.com>
2022-08-29 18:29:25 +0700

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import "./App.css";
+import { useState, useEffect } from 'react';
 
-function Movies () {
+function Movie({searchTitle}) {
+	if(searchTitle === '') searchTitle = 'spongebob';
+	const fetchString = 'http://www.omdbapi.com/?apikey=2e1e970c&s=' + searchTitle;
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		fetch(fetchString)
+		.then(res  => res.json())
+		.then(data => setData(data.Search));
+	}, [fetchString]);
+
   return (
-    <div>Movies</div>
-  )
+  <div class="flex-container">
+		{
+	    data === undefined ? <h1><font color="white">Movie title not found</font></h1> :
+		(data.map((d,index) => {
+			return(
+				<div class="img-container">
+					<img src={d.Poster} alt={d.Title} />
+					<div class="text-block">
+						<span><b>{d.Title.substring(0,40)}</b></span>
+					</div>
+				</div>
+			)
+		}
+		))
+		}
+  </div>
+  );
 }
 
 export default Movies
